3af9d78f05f25ca1046cd0b3673cf70ff59ec402
fdashx <35153328+fdashx@users.noreply.github.com>
2021-04-10 16:23:36 +1000

    add gitignore and main

---
+++ main.py
@@ -0,0 +1,19 @@
+def process_cmds(cmd: str) -> bool:
+    cmd_lower = cmd.lower()
+
+    if cmd_lower == "q":
+        return False
+    else:
+        print_help()
+        return True
+
+
+def print_help():
+    print("Usage:")
+    print("q - Quit")
+
+
+if __name__ == "__main__":
+    while process_cmds(input("Enter command: ")):
+        pass
+
